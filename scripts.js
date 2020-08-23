89fd94b64068ec354b68699fc81c7cb441428919
Marcos Pozzo <marcospozzo@MacBook-Air-de-Marcos.local>
2020-08-23 02:09:25 -0300

    logic game added

--- scripts.js
+++ scripts.js
@@ -1,11 +1,53 @@
 const cards = document.querySelectorAll(".card");
-cards.forEach((card) => card.addEventListener("click", flipCard));
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-  this.classList.toggle("flip");
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 (function shuffle() {
@@ -14,3 +56,5 @@ function flipCard() {
     card.style.order = randomPos;
   });
 })();
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
